f2997c7f1069190aa1cadd079ce3202aa2623477
Bayrmagnai99461124 <bayrmagnai.1124@gmail.com>
2025-03-12 14:16:44 +0800

    Update da-5.py

--- da-5.py
+++ da-5.py
@@ -1,8 +1,8 @@
-tsag_1 = int(input("tamirchin 1-iin tsag(sec):"))
-tsag_2 = int(input("tamirchin 2-iin tsag(sec):"))
-tsag_3 = int(input("tamirchin 3-iin tsag(sec):"))
+tsag_1 = int(input())
+tsag_2 = int(input())
+tsag_3 = int(input())
 niit_tsag = tsag_1 + tsag_2 + tsag_3
 
 minute = niit_tsag // 60
 secund = niit_tsag % 60
-print(f"niit hugatsaa: {minute:02}:{secund:02}")
+print(f" {minute:02}:{secund:02}")
